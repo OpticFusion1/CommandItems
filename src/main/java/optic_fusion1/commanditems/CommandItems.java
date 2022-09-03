b4c51e4ebcff8556822c3a6c605bff6fa742a2c8
OpticFusion1 <37254722+OpticFusion1@users.noreply.github.com>
2022-09-03 02:20:33 -0400

    Version 0.1.0 Plugin now loads

---
+++ src/main/java/optic_fusion1/commanditems/CommandItems.java
@@ -0,0 +1,11 @@
+package optic_fusion1.commanditems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommandItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
